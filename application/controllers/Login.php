9862083f92d98ccaec428aaaf161bcc786247143
Padmanaban <pathprabu16@gmail.com>
2016-01-18 16:55:29 +0530

    small change

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -10,14 +10,14 @@ class Login extends CI_Controller {
   {
     parent::__construct();
     
-    /* Load the libraries and helpers */
+ 
     $this->load->library('form_validation');
     $this->load->library('Session');
     $this->load->helper(array('form', 'url', 'captcha','string'));
   }
    
    
-  /* The default function that gets called when visiting the page */
+
 	public function index()
 	{
     
@@ -34,21 +34,21 @@ class Login extends CI_Controller {
       
       
       
-    /* Check if form (and captcha) passed validation*/
+  
     if ($this->form_validation->run() == TRUE &&
         strcmp(strtoupper($userCaptcha),strtoupper($word)) == 0)
     {
-      /** Validation was successful; show the Success view **/
+    
       
       
-      /* Clear the session variable */
+   
       $this->session->unset_userdata('captchaWord');
       
       
-      /* Get the user's name from the form */
+    
       $name = set_value('name');
     
-      /* Pass in the user input to the success view for display */
+     
       $data = array('name' => $name);
       $this->load->view('success-view', $data);
 
@@ -57,11 +57,11 @@ class Login extends CI_Controller {
     else 
     {
       
-      /** Validation was not successful - Generate a captcha **/
+ 
       $rand= random_string('alnum', 4);
       
       
-      /* Setup vals to pass into the create_captcha function */
+     
     $vals = array(
     'word'	=> $rand,
     'img_path'	=> './cap/',
@@ -80,10 +80,10 @@ class Login extends CI_Controller {
 $cap1 = create_captcha($vals);
       
       $k=array('cap'=>$cap1);
-      /* Store the captcha value (or 'word') in a session to retrieve later */
+     
       $this->session->set_userdata('captchaWord',$rand);
       
-      /* Load the captcha view containing the form (located under the 'views' folder) */
+   
       $this->load->view('captcha-view.php',$k);
     }
 		
